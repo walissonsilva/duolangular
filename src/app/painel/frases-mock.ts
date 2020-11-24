787ec850a986288af657f710c6fa20e5a582db9c
Walisson Silva <walissonsilva10@gmail.com>
2020-11-24 09:49:45 -0300

    README.md updated

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -2,7 +2,7 @@ import { Frase } from "../shared/frase.model";
 
 
 export const FRASES: Frase[] = [
-  { fraseEng: 'I want study', frasePtBr: 'Eu quero estudar' },
-  { fraseEng: 'I love Maiully', frasePtBr: 'Eu amo Maiully' },
+  { fraseEng: 'I am learning Angular', frasePtBr: 'Eu estou aprendendo Angular' },
   { fraseEng: 'I am programmer', frasePtBr: 'Eu sou programador' },
+  { fraseEng: 'I love my wife', frasePtBr: 'Eu amo minha esposa' },
 ];
